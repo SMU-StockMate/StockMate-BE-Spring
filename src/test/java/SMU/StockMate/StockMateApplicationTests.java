a8a9efbb8f2a5975584f80dd5ce5c4b7c44bd97c
donghyukseo <ehdgurdusdn@naver.com>
2025-06-26 13:23:17 +0900

    first commit

---
+++ src/test/java/SMU/StockMate/StockMateApplicationTests.java
@@ -0,0 +1,13 @@
+package SMU.StockMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
