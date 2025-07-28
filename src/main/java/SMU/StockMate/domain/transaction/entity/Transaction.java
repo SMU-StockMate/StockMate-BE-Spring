01f00e6d0669e8a52f79aec717f9a4599da9c853
donghyukseo <ehdgurdusdn@naver.com>
2025-07-28 18:38:46 +0900

    feat : transaction entity 추가

---
+++ src/main/java/SMU/StockMate/domain/transaction/entity/Transaction.java
@@ -0,0 +1,31 @@
+package SMU.StockMate.domain.transaction.entity;
+
+import SMU.StockMate.domain.transaction.enums.Type;
+import SMU.StockMate.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "transaction")
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Transaction extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long quantity; // 매수량
+
+    private Long price; // 매매가
+
+    @Column(name = "total_amount")
+    private Long totalAmount; // 총액
+
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+
+}
---
+++ src/main/java/SMU/StockMate/domain/transaction/enums/Type.java
@@ -0,0 +1,5 @@
+package SMU.StockMate.domain.transaction.enums;
+
+public enum Type {
+    BUY, SELL
+}
