4024d260f75f7718a8af517318a1c14a6a402399
donghyukseo <ehdgurdusdn@naver.com>
2025-07-28 21:47:49 +0900

    feat : stock, stockDaily entity 추가

---
+++ src/main/java/SMU/StockMate/domain/dailyStock/entity/DailyStock.java
@@ -0,0 +1,37 @@
+package SMU.StockMate.domain.dailyStock.entity;
+
+import SMU.StockMate.domain.stock.entity.Stock;
+import SMU.StockMate.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "daily_stock")
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class DailyStock extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "open_price")
+    private Long openPrice;
+
+    @Column(name = "close_price")
+    private Long closePrice;
+
+    @Column(name = "high_price")
+    private Long highPrice;
+
+    @Column(name = "low_price")
+    private Long lowPrice;
+
+    private Long volume; // 거래량
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stock_id")
+    private Stock stock;
+}
---
+++ src/main/java/SMU/StockMate/domain/stock/entity/Stock.java
@@ -0,0 +1,21 @@
+package SMU.StockMate.domain.stock.entity;
+
+import SMU.StockMate.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "stock")
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Stock extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "stock_code")
+    private String stockCode;
+}
