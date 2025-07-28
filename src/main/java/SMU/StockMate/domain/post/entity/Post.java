8bdcdadb0b4df01c90768aba5bbee324c7e6fc97
donghyukseo <ehdgurdusdn@naver.com>
2025-07-28 21:50:32 +0900

    refactor : post와 stock,users 연관관계 매핑

--- src/main/java/SMU/StockMate/domain/post/entity/Post.java
+++ src/main/java/SMU/StockMate/domain/post/entity/Post.java
@@ -1,5 +1,7 @@
 package SMU.StockMate.domain.post.entity;
 
+import SMU.StockMate.domain.stock.entity.Stock;
+import SMU.StockMate.domain.users.entity.Users;
 import SMU.StockMate.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -22,6 +24,12 @@ public class Post extends BaseEntity {
 
     private int likeCount; // 좋아요 수
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stock_id")
+    private Stock stock;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users_id")
+    private Users users;
 
 }
