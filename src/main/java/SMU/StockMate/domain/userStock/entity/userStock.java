5324037871bc6d698dfdd846984bade3c86f2837
donghyukseo <ehdgurdusdn@naver.com>
2025-07-28 19:28:10 +0900

    feat : userStock entity 추가

---
+++ src/main/java/SMU/StockMate/domain/userStock/entity/userStock.java
@@ -0,0 +1,32 @@
+package SMU.StockMate.domain.userStock.entity;
+
+import SMU.StockMate.domain.users.entity.Users;
+import SMU.StockMate.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "user_stock")
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class userStock extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String stockCode;
+
+    private Long quantity; // 보유 수량
+
+    @Column(name = "avg_price")
+    private Long avgPrice; // 평균 매수 금액
+
+    private Long returns; // 수익률
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users_id")
+    private Users users;
+}
