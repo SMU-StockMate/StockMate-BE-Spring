922cf218def7f46276e4e11056c7402d35a13a33
seonjiwon <tommys915@gmail.com>
2025-08-20 12:30:19 +0900

    refactor: 종목 검색 활성화 상태 반영

--- src/main/java/SMU/StockMate/domain/stock/query/repository/StockQueryRepository.java
+++ src/main/java/SMU/StockMate/domain/stock/query/repository/StockQueryRepository.java
@@ -12,17 +12,18 @@ import java.util.Optional;
 
 public interface StockQueryRepository extends JpaRepository<Stock, Long> {
     @Query("SELECT s FROM Stock s " +
-            "WHERE DATE(s.updatedAt) = :date " +
+            "WHERE s.isActivate = true " +
             "AND s.stockCode LIKE %:keyword% or s.koreanName LIKE %:keyword% ")
-    List<Stock> findByNameContaining(@Param("date")LocalDate date, @Param("keyword") String keyword, Pageable pageable);
+    List<Stock> findByNameContaining(@Param("keyword") String keyword, Pageable pageable);
 
     @Query("SELECT s FROM Stock s " +
-            "WHERE DATE(s.updatedAt) = :date " +
+            "WHERE s.isActivate = true " +
             "AND (s.stockCode LIKE %:keyword% OR s.koreanName LIKE %:keyword%) " +
             "AND s.id > :cursor ")
-    List<Stock> findByNameContainingNextPage(@Param("cursor") Long cursor, @Param("date") LocalDate date,
-                                             @Param("keyword") String keyword, Pageable pageable);
-    
-  Optional<Stock> findByStockCode(String stockCode);
-  
+    List<Stock> findByNameContainingNextPage(@Param("cursor") Long cursor,
+                                             @Param("keyword") String keyword,
+                                             Pageable pageable);
+
+    Optional<Stock> findByStockCode(String stockCode);
+
 }
